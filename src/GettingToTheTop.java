6f234c52fe8ac6efbe6c219f28d9f46738917635
Diego Vega <diegovega@me.com>
2023-07-24 11:56:55 +0200

    Add problem explanation

---
+++ src/GettingToTheTop.java
@@ -0,0 +1,11 @@
+public class GettingToTheTop {
+
+    /*
+    * As a new fitness tracking startup, we want to create a feature that counts the number of distinct ways
+    * our users can climb a ladder.  The ladder has n rungs and our users can climb either 1, 2 or 3 steps at a time.
+    * We need you to create a function that takes in the number of rungs and returns the number of distinct patterns
+    * our users can climb the ladder. This will enable us to track the progress of our users  and provide them with
+    * personalized fitness plans.
+    *
+    * */
+}
